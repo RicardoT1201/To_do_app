7f99adf27ecf90dab0070bea81d39ecdbf0ed03f
RicardoT1201 <158579507+RicardoT1201@users.noreply.github.com>
2024-03-05 12:38:18 +0100

    to do list

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.apache.tools.ant.util.JavaEnvUtils.VERSION_1_8
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.todoapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.todoapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    val sqliteVersion = "2.4.0"
+
+    // SQLite
+    implementation("androidx.sqlite:sqlite-ktx:$sqliteVersion") // Kotlin
+    //implementation("androidx.sqlite:sqlite-framework:$sqliteVersion") // Implementation of the AndroidX SQLite interfaces via the Android framework APIs.
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
